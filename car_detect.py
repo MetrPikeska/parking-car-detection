cfcb6dd08fa9689c0ee208dbefaff20ae5c92062
MetrPikeska <piter.mikeska@gmail.com>
2026-01-31 01:16:06 +0100

    Initial commit: car detection script

---
+++ car_detect.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('yolov8n.pt')
+
+video_path = 'input_video.mp4'
+cap = cv2.VideoCapture(video_path) #vytvori capture objekt z videa "ctecka videa" 
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    for r in results:
+        for box in r.boxes:
+            cls = int(box.cls[0])
+            label = model.names[cls]
+
+            if label == "car":
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+                cv2.putText(frame, "CAR", (x1, y1-5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+
+    cv2.imshow("Detekce aut", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC
+        break
+
+cap.release()
+cv2.destroyAllWindows()
